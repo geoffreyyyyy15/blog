3eabe3ff6cde28790b2b0d1b9643c4b9addc0d07
Hexamillion <48009181+geoffreyyyyy15@users.noreply.github.com>
2023-03-16 16:32:25 +0800

    Comment Post, Search added,  and bug fixes

--- resources/views/home/posts.blade.php
+++ resources/views/home/posts.blade.php
@@ -1,2 +1,32 @@
+<x-layout>
+    <x-nav />
+<<<<<<< HEAD
+    <div class="mr-5">
+        <div class="d-flex ">
+          <x-side-panel />
+            <x-flash />
+            <div class="">
+            <div class="ml-5">
+                <h2 class="text-white text-xl text-center">My Posts</h2>
+            </div>
+            <x-post-message />
+                @php
+                $slug = 1;
+            @endphp
+                <x-posts :users="$users" :slug="$slug" />
+                <div class="ml-5">
+                {{ $users->links() }}
+                </div>
+        </div>
+    </div>
+</div>
+</x-layout>
+=======
+    <x-side-panel />
+
+    <x-posts :users="$users"  />
+</div>
+
 
 </x-layout>
+>>>>>>> 57d7635a5fc2bede6b73602d4e5ea732ba123afc
